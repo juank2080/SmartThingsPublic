8abbb2e339b9e52742ba5592cb308622384b8556
Juan Romero <juank2080@gmail.com>
2020-04-27 20:08:57 -0400

    Added more automation

--- smartapps/juank2080/check-house.src/check-house.groovy
+++ smartapps/juank2080/check-house.src/check-house.groovy
@@ -28,14 +28,19 @@ preferences {
 	section("Side door check:") {
     	input "sideDoorSensor", "capability.contactSensor", title: "Side door sensor?", required: true, multiple: false
         input "sideDoorLock", "capability.lock", title: "Side door lock?", required: true, multiple: false
-        input "minInterval", "number", title: "Check in between (min)", required: true, multiple: false
+        input "checkDoorInterval", "number", title: "Check door each: (min)", required: true, multiple: false
+        input "closeDoorDelayAfterSensorClosed", "number", title: "After sensor closes, lock door after: (sec)", required: true, multiple: false
 	}
-    section("Outdoor lights check:") {
+    section("Go Bed Home Routine - Lights:") {
         input "lightsIndoorRef", "capability.light", multiple: true, title: "Indoor Lights as Refertence"
         input "lightsOutdoorRef", "capability.light", multiple: true, title: "Outside Lights to check"
-        input "timeOfDay", "time", title: "Time?"
+        input "timeOfDayLights", "time", title: "Time?"
         input "checkInterval", "number", title: "Check in between (min)", required: true, multiple: false
         input "maxAttempts", "number", title: "Max Attempts", required: true, multiple: false
+    }
+    section("Go Bed Home Routine - Locks:") {
+        input "doorLocks", "capability.lock", title: "Locks to check?", required: true, multiple: true
+        input "timeOfDayLocks", "time", title: "Time?"
     }
 }
 
@@ -46,74 +51,102 @@ def installed() {
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+    log.debug "Updated with settings: ${settings}"
 
 	unsubscribe()
 	unschedule()
-    
+
 	initialize()
 }
 
 def initialize() {
 	log.debug "initialize"
+  
+	// My App event subscriptions  
+	subscribe(sideDoorLock, "lock.unlocked", doorLockOnUnlocked)
+	subscribe(sideDoorSensor, "contact.closed", contactSensorOnClose)
     
-    log.debug "minInterval ${minInterval}"
+    // Creating schedulers using timezone 
+	def start = timeToday(timeOfDayLights, location?.timeZone)
+	schedule(start, checkLights);
     
-    subscribe(sideDoorLock, "lock.unlocked", startCheckingSideSensor)
-    def start = timeToday(timeOfDay, location?.timeZone)
-    log.debug "Local time ${start} or ${timeOfDay}"
-    schedule(start, checkLights);
+    start = timeToday(timeOfDayLocks, location?.timeZone)
+	schedule(start, checkLocks);
     
+    // Check if door is currently "unlocked"
     def currentValue = sideDoorLock.currentValue("lock")
-    log.debug "Current lock status ${currentValue}"
-    if (currentValue != "locked") {
-    	runIn(60*minInterval, checkSideDoorSensor);
+    log.debug "Current values: Door (${sideDoorLock.currentLock}), Contact: (${sideDoorSensor.currentContact})"
+    if (sideDoorLock.currentLock != "locked" && sideDoorSensor.currentContact == "closed") {
+    	log.debug "Setting timer to check door again in ${minInterval} min"
+    	runIn(60 * checkDoorInterval, checkSideDoorSensor);
     }
     
+    // Init counter
     state.lightAttempt = 0
 }
 
-def startCheckingSideSensor(evt) {
-	log.debug "startCheckingSideSensor ${evt}"
-	runIn(60*minInterval, checkSideDoorSensor);
+/**
+* START EVENT HANDLERS
+*/ 
+def contactSensorOnClose(evt){
+	log.debug "[contactSensorOnClose] checking in ${checkDoorInterval} min"
+    
+    log.debug "[contactSensorOnClose] Side door lock status in ${sideDoorLock.currentLock}"
+	if (sideDoorLock.currentLock != "locked") {
+    	log.debug "[contactSensorOnClose] Removing scheduler checkSideDoorSensor"
+    	unschedule(checkSideDoorSensor)
+        log.debug "[contactSensorOnClose] Closing door in ${closeDoorDelayAfterSensorClosed} sec"
+    	runIn(closeDoorDelayAfterSensorClosed, closeSideDoor)
+    }
 }
 
+def closeSideDoor(){
+	sideDoorLock.lock()
+}
+
+def doorLockOnUnlocked(evt) {
+	log.debug "[doorLockOnUnlocked] checking in ${checkDoorInterval} min"
+	runIn(60 * checkDoorInterval, checkSideDoorSensor);
+}
+/*** END EVENT HANDLERS ***/
+
 def checkSideDoorSensor() {
-	def currentValue = sideDoorSensor.currentValue("contact")
-    log.debug "the current value of sideDoorSensor is ${currentValue}"
+	log.debug "[checkSideDoorSensor] The current value of sideDoorSensor is ${sideDoorSensor.currentContact}"
     
-	if (currentValue == "closed") {
-    	log.debug "Locking side door"
+	if (sideDoorSensor.currentContact == "closed") {
+		log.debug "[checkSideDoorSensor] Locking side door"
 		sideDoorLock.lock()
 	} else {
-    	runIn(60*minInterval, checkSideDoorSensor, [overwrite: false]);
-        log.debug "Executing in ${minInterval}min"
+    	log.debug "[checkSideDoorSensor] Executing checkSideDoorSensor in ${checkDoorInterval} min"
+    	runIn(60 * checkDoorInterval, checkSideDoorSensor, [overwrite: false]);
   	}
 }
 
-def checkLights() {
-	log.debug "checkStatus"
+def checkLocks(){
+	log.debug "[checkLocks] Locking doors..."
+    
+	doorLocks.findAll { lock -> lock.currentLock != "locked" }?.each { lock -> lock.lock() }
+}
 
-	def outdoorLightsStatus = lightsOutdoorRef.currentSwitch
+def checkLights() {
+	log.debug "[checkLights] Checking ..."
     
-    log.debug "outdoorLightsStatus ${outdoorLightsStatus.size()}"
-    
-	if (outdoorLightsStatus?.any { switchVal -> switchVal == "on" ? true : false }) {
-    	log.debug "Found at least one outdoor light on"
+	if (lightsOutdoorRef?.any { light -> light.currentSwitch == "on" }) {
+    	log.debug "[checkLights] Found at least one outdoor light on"
         
-        def indoorLightsStatus = lightsIndoorRef.currentSwitch
-        if (state.lightAttempt < maxAttempts && indoorLightsStatus?.any { switchVal -> switchVal == "on" }) {
+        if (state.lightAttempt < maxAttempts && lightsIndoorRef?.any { light -> light.currentSwitch == "on" }) {
             state.lightAttempt = state.lightAttempt + 1
-            log.debug "It is at least one light indoor on, check again in ${checkInterval}min, attempt: ${state.lightAttempt}"            
+            log.debug "[checkLights] It is at least one light indoor on, check again in ${checkInterval}min, attempt: ${state.lightAttempt}"
+            
 			runIn(60*checkInterval, checkLights, [overwrite: false]);
       	} else {
         	state.lightAttempt = 0
-            log.debug "Any indoor light is on, turning lights off"
+            log.debug "[checkLights] Any indoor light is on, turning lights off"
             lightsOutdoorRef.findAll { light -> light.currentSwitch == "on" }?.each { light -> light.off() }
         }
         
     } else {
-    	log.debug "Any light on - sleeping"
+    	log.debug "[checkLights] No outside lights on - stop checking"
         state.lightAttempt = 0
     }
 }
